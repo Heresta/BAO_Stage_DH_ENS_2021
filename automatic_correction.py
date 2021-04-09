106b3b8c5010d0170e217882b1ed5cf0731b841e
Juliette Janes <juliette.janes@yahoo.fr>
2021-04-09 16:24:25 +0200

    Réglement de l'échec

--- automatic_correction.py
+++ automatic_correction.py
@@ -4,21 +4,23 @@ import click
 import re
 from lxml import etree as ET
 import errno
-#récupérer le dossier à transformer
+
+#récupérer le dossier à transformer et le dossier résultat
 chemin = input("entrez le chemin du dossier: ")
 dossier_resultat = input("entrez le chemin du dossier résultat: ")
+# si le dossier résultat n'existe pas, on le créé, sinon, on ne fait rien
 if not os.path.exists(os.path.dirname(dossier_resultat)):
     try:
         os.makedirs(os.path.dirname(dossier_resultat))
     except OSError as exc:  # Guard against race condition
         if exc.errno != errno.EEXIST:
             raise
+
 for fichier in os.listdir(chemin):
-    original = ET.parse(fichier)
-
-    transformation_xlst = ET.XSLT(ET.parse("./transformation_xml_corrige_escriptorium.xsl"))
-
+    # pour chaque fichier du dossier, on applique la feuille de transformation de correction
+    original = ET.parse(chemin+fichier)
+    transformation_xlst = ET.XSLT(ET.parse("migrationCorrection.xsl"))
     propre = transformation_xlst(original)
-
-    with open('./nettoyage/'+fichier, mode='wb') as f:
+    #on créé un nouveau fichier dans le dossier résultat
+    with open(dossier_resultat+fichier, mode='wb') as f:
         f.write(propre)
